3ebf9ec01707f3cce2e5463c8c83bf49b516f76c
Marcus <marcus1337>
2024-09-20 14:42:49 +0200

    start script

--- scripts/start.sh
+++ scripts/start.sh
@@ -17,15 +17,17 @@ ray start --head --node-ip-address=${HEAD_NODE_IP} --port=6379 --num-cpus=0 --nu
 cleanup() {
     echo "Stopping all child processes..."
     ray stop
-    kill 0 
+    kill $MAIN_PID $SERVICE_PID
+    wait $MAIN_PID $SERVICE_PID 
     exit 0
 }
 trap cleanup SIGINT SIGTERM
 
 SCRIPT_DIR=$(dirname "$0")
 python "$SCRIPT_DIR/../autoscaler/main.py" &
+AUTOSCALER_PID=$!
 python "$SCRIPT_DIR/../hypha/service.py" &
+SERVICE_PID=$!
 
-
-wait
+wait $MAIN_PID $SERVICE_PID
 

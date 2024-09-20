32ebe4f11bb9cd121d228e2893250285f9f09211
Marcus <marcus1337>
2024-09-20 10:38:14 +0200

    start script

--- scripts/start.sh
+++ scripts/start.sh
@@ -4,8 +4,15 @@ module load Mambaforge/23.3.1-1-hpc1-bdist
 module load buildenv-gcccuda/12.1.1-gcc12.3.0
 
 ENV_NAME="ray_env"
+HEAD_NODE_IP=$(hostname -I | awk '{print $1}')
+
 conda info --envs | grep -q "^${ENV_NAME} " || mamba create --name ${ENV_NAME} python=3.9
 mamba activate ${ENV_NAME}
-pip install ray
+pip install "ray[all]"
 pip install hypha-rpc
+
+ray stop
+ray start --head --node-ip-address=${HEAD_NODE_IP} --port=6379 --num-cpus=0 --num-gpus=0
+
+
 

47495a7ba317139a5eb0633f698798f0be8b43a5
Marcus <marcus1337>
2024-10-02 13:03:24 +0200

    token retrieval script

--- scripts/token.sh
+++ scripts/token.sh
@@ -1,17 +1,14 @@
 #!/bin/bash
 
-echo "hello test"
-
 SCRIPT_DIR=$(dirname "$0")
 SCRIPT_PATH="$SCRIPT_DIR/../hypha/token_init.py"
+output=$(python -u "$SCRIPT_PATH" | tee /dev/tty)
 
-python "$SCRIPT_PATH"
-
-# Check if the output matches the regex for "export x=y"
-if [[ $output =~ ^export\ [a-zA-Z_][a-zA-Z0-9_]*=.+$ ]]; then
-    echo "Output is valid: $output"
-    # You can optionally evaluate the output to set the environment variable
-    eval "$output"
+# Check if the last line of output matches the regex for "export x=y"
+if [[ $output =~ (export\ [a-zA-Z_][a-zA-Z0-9_]*=[^ ]+) ]]; then
+    export_variable="${BASH_REMATCH[1]}"
+    echo "Output is valid: $export_variable"
+    eval "$export_variable"
 else
     echo "FAIL"
     exit 1  # Return a non-zero exit status to indicate failure

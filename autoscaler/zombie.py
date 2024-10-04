fc9a6def95871d49f84d5056823ba8ba2eb8c9b1
Marcus <marcus1337>
2024-10-04 14:30:04 +0200

    zombie class

--- autoscaler/zombie.py
+++ autoscaler/zombie.py
@@ -1,7 +1,9 @@
 from datetime import datetime
 from status import Status
 from config import Config
+from typing import List
 import terminal
+import os
 
 class ZombieTerminator:
     
@@ -9,9 +11,15 @@ class ZombieTerminator:
         self.status = status
         self.last_work_time = datetime.now()
 
-    def _terminate_jobs(self):
-        #terminal.run_command()
-        ...
+    @staticmethod
+    def _get_job_ids() -> List[str]:
+        user = os.getenv("USER")
+        job_ids_output = terminal.run_command(["squeue", "-u", user, "-h", "-o", "%A"])    
+        return job_ids_output.splitlines()
+
+    def _terminate_jobs(self, job_ids: List[str]):
+        for job_id in job_ids:
+            terminal.run_command(["scancel", job_id])
 
     def _are_workers_zombies(self) -> bool:
         return not self.status.ray_metrics.is_work_ongoing() and self.status.job_metrics.total_jobs > 0
@@ -27,4 +35,4 @@ class ZombieTerminator:
         if self.status.ray_metrics.is_work_ongoing():
             self.last_work_time = datetime.now()
         if self._should_terminate_jobs():
-            self._terminate_jobs()
+             self._terminate_jobs(ZombieTerminator._get_job_ids())
